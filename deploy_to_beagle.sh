c2b8a5470b6676f8bcea416cabb959b3ac40d8de
Jeffrey McAteer <jeffrey.p.mcateer@gmail.com>
2019-06-22 02:11:50 -0400

    add beaglebone server deployment script

--- deploy_to_beagle.sh
+++ deploy_to_beagle.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+# Using a seperate staging area solves performance problems where
+# the docker container blows away my x86 caches and replaces them with armv7 caches.
+# #rude.
+
+mkdir -p /tmp/dindex-armv7-staging
+rsync -av --progress . /tmp/dindex-armv7-staging --exclude target || exit 1
+cd /tmp/dindex-armv7-staging || exit 1
+
 cross build --release --bin dindex-server --target armv7-unknown-linux-musleabihf || exit 1
 
 ssh beagle sudo systemctl stop dindex
