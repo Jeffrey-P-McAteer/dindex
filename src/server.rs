69277106e0a3a59ced9a1b128224cf108adc0716
Jeffrey McAteer <jeffrey.p.mcateer@gmail.com>
2019-10-14 11:11:51 -0400

    super simple implementation of JSON record store

--- src/server.rs
+++ src/server.rs
@@ -25,6 +25,7 @@ use std::io::prelude::*;
 
 use crate::config::Config;
 use crate::data::Data;
+use crate::record::Record;
 
 pub fn run_sync(config: &Config) {
   let mut data = Data::new(config);
@@ -78,7 +79,7 @@ fn read_stored_records(config: &Config, data: &mut Data) {
             read_stored_records_json_file(file, data);
           }
           else {
-            println!("Error: unknown filetype {}", path);
+            println!("Error: reading server_datastore_uri; unknown filetype '{}'", path);
           }
         }
       }
@@ -92,6 +93,68 @@ fn read_stored_records(config: &Config, data: &mut Data) {
   }
 }
 
-fn read_stored_records_json_file(json_f: File, data: &mut Data) {
-  
+fn write_stored_records(config: &Config, data: &mut Data) {
+  let uri_s = &config.server_datastore_uri;
+  if let Ok(uri) = Url::parse(uri_s) {
+    match uri.scheme() {
+      "file" => {
+        let path = uri.path();
+        // This function will create nonexisting files, and truncate existing files when data is written
+        if let Ok(file) = File::create(path) {
+          if path.contains(".json") {
+            write_stored_records_json_file(file, data);
+          }
+          else {
+            println!("Error: reading server_datastore_uri; unknown filetype '{}'", path);
+          }
+        }
+      }
+      unk => {
+        println!(
+          "Error reading in data: unknown scheme '{}' in given server_datastore_uri={}",
+          unk, config.server_datastore_uri
+        );
+      }
+    }
+  }
 }
+
+fn read_stored_records_json_file(mut json_f: File, data: &mut Data) {
+  let mut contents = String::new();
+  if let Err(e) = json_f.read_to_string(&mut contents) {
+    println!("read_stored_records_json_file: {}", e);
+    return;
+  }
+  
+  if let Ok(records) = serde_json::from_str::<Vec<Record>>(&contents) {
+    for rec in records {
+      data.insert(rec);
+    }
+  }
+}
+
+fn write_stored_records_json_file(mut json_f: File, data: &mut Data) {
+  // TODO can we serialize without cloning everything OR without locking everything?
+  let mut records = vec![];
+  for pool in data.record_pools.iter() {
+    let mut read_retries = 5;
+    for _ in 0..read_retries {
+      if let Ok(pool) = pool.try_read() {
+        for rec in pool.iter() {
+          records.push(rec.clone());
+        }
+        break;
+      }
+    }
+  }
+  
+  let records_json_s = serde_json::to_string(
+    &records
+  ).expect("Cannot serialize a record");
+  
+  if let Err(e) = json_f.write_all(records_json_s.as_bytes()) {
+    println!("Unable to write new data to db: {}", e);
+  }
+}
+
+
