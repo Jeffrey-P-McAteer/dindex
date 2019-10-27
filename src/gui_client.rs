e1d97b7124fe26308e72e7ec50fc96c40d5ab8ea
Jeffrey McAteer <jeffrey.p.mcateer@gmail.com>
2019-10-26 21:42:54 -0400

    I hate the theory but it actually looks and performs well

--- src/gui_client.rs
+++ src/gui_client.rs
@@ -17,28 +17,34 @@
  *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-use piston::window::WindowSettings;
-use piston::event_loop::{Events, EventSettings};
-use piston_window::{Event, Input};
-use glutin_window::GlutinWindow;
+use web_view::*;
+
+use crossbeam_utils::thread;
+
 
 use crate::config;
 
 pub fn run_sync(config: &config::Config) {
-  let settings = WindowSettings::new("dIndex", (600, 400))
-        .exit_on_esc(true);
-  let mut window: GlutinWindow = settings.build()
-        .expect("Could not create window");
-  let mut events = Events::new(EventSettings::new());
-  while let Some(e) = events.next(&mut window) {
-    match e {
-      // Event::Input(Input::Resize(rs), _option_timestamp) => {
-      //   rs.window_size
-      // }
-      unk_e => {
-        println!("unk_e={:?}", unk_e);
-      }
-    }
-  }
+  thread::scope(|s| {
+    let h1 = s.spawn(move |_| {
+      crate::http_client::run_sync(config);
+    });
+    
+    let h2 = s.spawn(move |_| {
+      web_view::builder()
+        .title("dIndex")
+        .content(Content::Url(format!("http://127.0.0.1:{}", config.client_http_port)))
+        .size(800, 600)
+        .resizable(true)
+        .debug(true)
+        .user_data(())
+        .invoke_handler(|_webview, _arg| Ok(()))
+        .run()
+        .unwrap();
+    });
+    
+    h1.join().unwrap();
+    h2.join().unwrap();
+  }).unwrap();
 }
 
