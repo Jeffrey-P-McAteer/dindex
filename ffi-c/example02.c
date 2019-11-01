49fa7af3f6fb787b10c343f4e36e770ef46fedc6
Jeffrey McAteer <jeffrey.p.mcateer@gmail.com>
2019-10-31 21:33:48 -0400

    added exit condition for example02

--- ffi-c/example02.c
+++ ffi-c/example02.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
-
+#include <stdlib.h> 
+#include <time.h>
 #include <dindex.h>
 
 int main(int argc, char** argv) {
+  // We use rand() to exit after receiving at least 1 record.
+  srand(time(0));
+  
   Config* config = dindex_config(NULL /* alternatively give result of dindex_args() */);
   Record* query = dindex_record_empty();
   dindex_record_put(query, "url", ".*");
@@ -14,7 +18,13 @@ int main(int argc, char** argv) {
   dindex_client_listen_sync(config, query, DINDEX_LAMBDA(const char* _(Record* result) {
     dindex_record_display(config, result);
     dindex_record_free(result);
-    return "Continue"; // must match one of client::ListenAction variants
+    if (rand() & 1) {
+      return "Continue"; // must match one of client::ListenAction variants
+    }
+    else {
+      printf("Stopping...\n");
+      return "EndListen";
+    }
   }));
   
   // Cleanup
